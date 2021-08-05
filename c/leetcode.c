3f5447cc98cd2c59402ccb88606bd4532921697b
chen_hua_you <chen.hua.you@163.com>
2021-08-05 09:44:26 +0800

    合并两个有序链表

--- c/leetcode.c
+++ c/leetcode.c
@@ -23,10 +23,12 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
         if(l1==NULL) {
             tail->next = l2;
             l2 = l2->next;
+            continue;
         }
         if(l2==NULL) {
             tail->next = l1;
             l1 = l1->next;
+            continue;
         }
         if(l1->val>l2->val){
             tail->next = l2;
